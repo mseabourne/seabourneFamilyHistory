66cf8a782a59b6bfcf1ed3f0de02bbccc763a025
provmark <mark.seabourne@providence.org>
2018-11-09 23:33:51 -0800

    Revert "Couple more tweaks to cleanup."
    This reverts commit 23b1c5ad64e64ecf404d1de0591101580cdfc4b0.

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,10 +1,10 @@
 module.exports = {
-  siteTitle: "Seabourne History", // Site title.
+  siteTitle: "Seabourne Family History", // Site title.
   siteTitleShort: "Seabourne Family", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: "Family History", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
   siteUrl: "https://vagr9k.github.io", // Domain of your website without pathPrefix.
-  pathPrefix: "/seabourneFamilyHistory", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
+  pathPrefix: "/blog", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
   siteDescription: "A blog to track Seabourne family history.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
